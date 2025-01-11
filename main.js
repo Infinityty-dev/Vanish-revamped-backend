4e84b23a802ff98c1991c82d4d47b530dbd17145
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2025-01-11 06:35:33 -0800

    cors and body-parser dependencies added

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const database = require('./database/config.js');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const router = require('./route/UserAndDriverRoute.js');
-const movementAndServiceOption = require('./route/userMovementDetailRoute.js')
+const movementAndServiceOption = require('./route/userMovementDetailRoute.js');
 
-
+//server
 const server = express();
 const port = 5500;
 
-
-database()
+// Middleware
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors());
 server.use(express.json());
 
+//database
+database()
 
+
+// Routes
 server.use('/api/v1', movementAndServiceOption)
 server.use('/api/v1',router)
 
