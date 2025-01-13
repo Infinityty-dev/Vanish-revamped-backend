e93193c6b0507807c68ac9a93e98ce27301a30d4
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2025-01-13 04:33:50 -0800

    changed the S in service option and service to s

--- controllers/userMovementAndServiceOptionsDetailsController.js
+++ controllers/userMovementAndServiceOptionsDetailsController.js
@@ -1,6 +1,6 @@
 
 const TestUser = require("../schema/TestUser.js");
-const serviceModel = require('../schema/serviceOptionAndMovementDetailsSchema.js');
+const serviceModel = require('../schema/ServiceOptionAndMovementDetailsSchema.js');
 const mongoose = require('mongoose');
 
 
